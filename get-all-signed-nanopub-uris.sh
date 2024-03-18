7caf4151fc529179deea2dcb8272accd15b84659
Tobias Kuhn <kuhntobias@gmail.com>
2024-03-18 13:31:40 +0100

    Change URL to knowledgepixels.com

--- get-all-signed-nanopub-uris.sh
+++ get-all-signed-nanopub-uris.sh
@@ -7,7 +7,7 @@ rm -f out/all-signed-nanopub-uris.latest.txt
 
 for PAGE in {1..50}; do
   echo "Downloading page $PAGE"
-  curl -s -H "Accept: text/csv" "https://grlc.nps.petapico.org/api/local/local/find_signed_nanopubs?page=$PAGE" \
+  curl -s -H "Accept: text/csv" "https://grlc.nps.knowledgepixels.com/api/local/local/find_signed_nanopubs?page=$PAGE" \
     | sed 1d \
     | awk -F, '{print $1}' \
     | sed 's/"//g' \
